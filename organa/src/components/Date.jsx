ec1c5cffdf4f4a19fdf5264ca89b82f290559551
MarinaEstefania <m.garciac11@gmail.com>
2019-05-29 15:13:05 -0500

    first try date

---
+++ organa/src/components/Date.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+class Date extends React.Component{
+
+    render(){
+
+        return(
+            <div>
+                <h5>{this.props.date}</h5>
+            </div>
+        )
+    }
+}
+
+export default Date;
+
--- organa/src/components/Navigation.jsx
+++ organa/src/components/Navigation.jsx
@@ -3,6 +3,16 @@ import { Navbar, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import LogoL from '../assets/Laboratorialogo.png';
 import styled from "styled-components";
+import Date from './Date'
+
+
+
+// Convert string '2014-02-11T11:30:30' to date:
+//var result = parse('2014-02-11T11:30:30')
+//=> Tue Feb 11 2014 11:30:30
+
+var isToday = require('date-fns/parse')
+
 
 const Styles = styled.div`
   .navbar {
@@ -12,6 +22,17 @@ const Styles = styled.div`
 
 class Navigation extends React.Component {
   
+  constructor (){
+    super()
+    this.state = {
+        date: 'date'
+    }
+    var today = new Date()
+    console.log( today)
+    console.log(new Date())
+    console.log( isToday(today))
+}
+
   render() {
     return (
   <Styles>
@@ -24,6 +45,11 @@ class Navigation extends React.Component {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
+       <Nav.Item>
+            
+              <Date  date={this.state.date}/> 
+         
+          </Nav.Item> 
           <Nav.Item>
             <Nav.Link>
               <Link to="/">Scanner</Link>
