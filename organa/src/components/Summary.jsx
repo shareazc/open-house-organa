1c9dc9e035d0d56cf8baf890f99054828f027cc6
angie <angyalva16@gmail.com>
2019-05-29 13:22:29 -0500

    Function for pulling information's firebase

--- organa/src/components/Summary.jsx
+++ organa/src/components/Summary.jsx
@@ -1,15 +1,42 @@
 import React from 'react';
-import Attendance from './Attendance'
-import Absence from './Absence'
+import firebase from '../firebase/FirebaseConfig';
+import Attendance from './Attendance';
+import Absence from './Absence';
+import {Container, Row, Col} from 'react-bootstrap';
+
 
 
 class Summary extends React.Component{
+
+    constructor(){
+        super();
+        this.state = {
+            date: [],
+            list: []
+        }
+    }
+
+    componentDidMount(){
+        const attendanceList = firebase.database().ref('attendance');
+        attendanceList.on('value', (s) =>{
+            let attendanceStudents = s.val();
+            for(let attendanceStudent in attendanceStudents){
+                this.setState({
+                    date: attendanceStudents[attendanceStudent].date,
+                    list: attendanceStudents[attendanceStudent].list,
+                });
+            }
+        })
+    }
+
     render(){
         return(
-            <div>
-                <h1><Attendance/></h1>
-                <h2><Absence/></h2>
-            </div>
+            <Container>
+                <Row>
+                    <Col lg={8}><h1 ><Attendance date={this.state.date} list={this.state.list}/></h1></Col>
+                    <Col lg={4}><h2><Absence date={this.state.date} list={this.state.list}/></h2></Col>
+                </Row>
+            </Container>
         )
     }
 }
