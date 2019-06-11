df1373458353c0638ef8569c0f001e77fe8ef683
Share Azcárraga <shareni.azcarraga@gmail.com>
2019-06-11 14:35:38 -0500

    Corrected indentation

--- organa/src/App.js
+++ organa/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Scanner from './components/Scanner';
-import  Summary  from './components/Summary';
+import Summary from './components/Summary';
 import Navigation from './components/Navigation';
 import Success from './components/Success';
 import { NoMatch } from './components/NoMatch';
@@ -10,16 +10,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    
-     <Router>
-     <Navigation />
-       <Switch>
-         <Route exact path = "/" component = {Scanner} />
-         <Route path = "/summary" component = {Summary} />
-         <Route path = "/success" component = {Success} />
-         <Route component = {NoMatch} />
-       </Switch>
-     </Router>
+      <Router>
+        <Navigation />
+        <Switch>
+          <Route exact path="/" component={Scanner} />
+          <Route path="/summary" component={Summary} />
+          <Route path="/success" component={Success} />
+          <Route component={NoMatch} />
+        </Switch>
+      </Router>
     </div>
   );
 }
