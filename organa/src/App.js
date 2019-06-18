1cad63a7c86b10e7c4150e0b7ea27d191260f096
Share Azcárraga <shareni.azcarraga@gmail.com>
2019-06-18 10:30:46 -0500

    --

--- organa/src/App.js
+++ organa/src/App.js
@@ -7,28 +7,28 @@ import Success from './components/Success';
 import { NoMatch } from './components/NoMatch';
 import './App.css';
 
-class App extends React.Component{
+class App extends React.Component {
 
-  componentDidMount(){
-    
+  componentDidMount() {
+
   }
 
-  render(){
+  render() {
     return (
       <div className="App">
-       <Router>
-       <Navigation />
-         <Switch>
-           <Route exact path = "/" component = {Scanner} />
-           <Route path = "/summary" component = {Summary} />
-           <Route path = "/success" component = {Success} />
-           <Route component = {NoMatch} />
-         </Switch>
-       </Router>
+        <Router>
+          <Navigation />
+          <Switch>
+            <Route exact path="/" component={Scanner} />
+            <Route path="/summary" component={Summary} />
+            <Route path="/success" component={Success} />
+            <Route component={NoMatch} />
+          </Switch>
+        </Router>
       </div>
     );
   }
-  
+
 
 }
 
