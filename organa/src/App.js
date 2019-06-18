e681002f8960a5fa548cfacf389a055113cca6ca
MarinaEstefania <m.garciac11@gmail.com>
2019-06-18 10:43:23 -0500

    function added: create a new date the firt time the application has been opended in that day

--- organa/src/App.js
+++ organa/src/App.js
@@ -6,14 +6,32 @@ import Navigation from './components/Navigation';
 import Success from './components/Success';
 import { NoMatch } from './components/NoMatch';
 import './App.css';
+import moment from 'moment';
+import firebase from './firebase/FirebaseConfig';
 
 class App extends React.Component{
-
-  componentDidMount(){
-    
+  constructor(props){
+    super(props);
+    this.state = {
+        date:  moment().format('ll')
+    }
   }
+  componentDidMount(){
+      const doesDateExist = firebase.database()
+      .ref('attendance').child(this.state.date);
+      doesDateExist.on('value', snap=>{
+        let actualDate = snap.val();
+        if(actualDate==null){
+         let newDate = moment().format('ll');   
+          firebase.database().ref('attendance/' + newDate)
+          .set(newDate) 
+      }
+    })
+  }
+ 
 
   render(){
+ 
     return (
       <div className="App">
        <Router>
